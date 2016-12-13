e2bb9d6036603a529e41a3d53ee5d7c648178da8
iproduct <office@iproduct.org>
2016-12-13 14:30:18 +0200

    sorting-employees added

--- js-objects-02/js/array-new-methods.js
+++ js-objects-02/js/array-new-methods.js
@@ -1,12 +1,13 @@
+'use strict';
 /* 
  * Test JS 1.6 new Array Methods
  */
 
-//var array = [2, 5, 2, 9];
-//var index = array.indexOf(2, -3); // index is assigned 0
-//console.log("index of 2 is ", index);
-//index = array.indexOf(7); // index is assigned -1
-//console.log("index of 7 is ", index);
+array = [3, 5, 7, 2, 7, 2, 9];
+var index = array.indexOf(2, -5); // index is assigned 0
+console.log("index of 2 is ", index);
+index = array.lastIndexOf(2); // index is assigned -1
+console.log("index of 2 is ", index);
 
 //var array = [2, 2, 5, 9, 2];
 //var index = array.lastIndexOf(7);
@@ -14,21 +15,31 @@
 function isYoung(value, index, array) {
     return value < 45;
 }
-function doubleAge(value){
+function doubleAge(value) {
     return value * 2;
 }
-function ageNextYear(value){
+function ageNextYear(value) {
     return ++value;
 }
-function print(value, index){
+function print(value, index) {
     console.log(index, " -> ", value);
 }
-function sum(previousValue, currentValue, index, array){
-  return previousValue + currentValue;
+function sum(previousValue, currentValue, index, array) {
+    return previousValue + currentValue;
 }
-var result = [41, 20, 17, 52, 39]
-        .filter(isYoung)
-        .map(ageNextYear)
-        .reduce(sum, 0);
-//        .forEach(print);
-console.log("Sum = ", result);
+function average(accumulator, value, index, array) {
+    if (index < array.length - 1) {
+        return accumulator + value;
+    } else {
+        return (accumulator + value) / array.length;
+    }
+}
+
+var result = [41, 20, 17, 50, 39, 75]
+    // .some( function(value) { return !isYoung(value); } );
+    // .some( value => !isYoung(value) );
+    .filter(isYoung)
+    .map(ageNextYear)
+    // .reduce(average, 0);
+    .forEach(print);
+// console.log("YoungAvg = ", result);
